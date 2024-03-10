bf94fab5cfee1b586522c4c5acf39d6a810afc3e
coder114514 <98397499+coder114514@users.noreply.github.com>
2024-03-10 22:53:27 +0800

    real readline

--- emulator/src/Logger.cpp
+++ emulator/src/Logger.cpp
@@ -9,16 +9,16 @@ namespace casioemu {
     namespace logger {
         void Info(const char *format, ...) {
             // * TODO may introduce race condition
-            if (RL_ISSTATE(RL_STATE_TERMPREPPED))
+            // if (RL_ISSTATE(RL_STATE_TERMPREPPED))
                 rl_clear_visible_line();
             va_list args;
             va_start(args, format);
             vprintf(format, args);
             va_end(args);
-            if (RL_ISSTATE(RL_STATE_TERMPREPPED)) {
+            // if (RL_ISSTATE(RL_STATE_TERMPREPPED)) {
                 rl_on_new_line();
                 rl_redisplay();
-            }
+            // }
         }
     }
 } // namespace casioemu
