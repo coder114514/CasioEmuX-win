57057a6d0ac37c4c1e01102c735e65d9f1353277
coder114514 <98397499+coder114514@users.noreply.github.com>
2024-03-20 20:19:37 +0800

    use Makefile for emulator

--- emulator/build.sh
+++
@@ -1,5 +0,0 @@
-g++ -Wall -pedantic -std=c++14 -O2 \
--I ./include \
-./src/casioemu.cpp ./src/Emulator.cpp ./src/Logger.cpp  ./src/Chipset/CPU.cpp ./src/Chipset/CPUPushPop.cpp ./src/Chipset/MMURegion.cpp ./src/Chipset/CPUControl.cpp ./src/Chipset/CPUArithmetic.cpp ./src/Chipset/CPULoadStore.cpp ./src/Chipset/Chipset.cpp ./src/Chipset/MMU.cpp ./src/Chipset/InterruptSource.cpp ./src/Peripheral/BatteryBackedRAM.cpp ./src/Peripheral/Peripheral.cpp ./src/Peripheral/Keyboard.cpp ./src/Peripheral/Screen.cpp ./src/Peripheral/Timer.cpp ./src/Peripheral/StandbyControl.cpp ./src/Peripheral/ROMWindow.cpp ./src/Peripheral/Miscellaneous.cpp ./src/Gui/CodeViewer.cpp ./src/Gui/ui.cpp ./src/Data/ModelInfo.cpp ./src/Gui/imgui/imgui_impl_sdl2.cpp ./src/Gui/imgui/imgui_impl_sdlrenderer2.cpp ./src/Gui/imgui/imgui.cpp ./src/Gui/imgui/imgui_widgets.cpp ./src/Gui/imgui/imgui_tables.cpp ./src/Gui/imgui/imgui_draw.cpp -o ./casioemu.exe \
--L ./lib \
--static-libgcc -static-libstdc++ -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -llua53 -lreadline -lhistory -ltermcap
